9334a23273ffc78ef1e9f485cd4acb5dc241a71e
eusouregislima <danilolima.cogic@gmail.com>
2022-07-29 16:41:20 -0300

    Atualização na aparência e no contador

--- script.js
+++ script.js
@@ -1,130 +1,163 @@
-const changeImgOne = document.getElementById("firstImg")
-const pcImg = document.getElementById('secondImg')
 const firstButton = document.getElementById('buttonStone')
 const secondButton = document.getElementById('buttonPaper')
 const thirdButton = document.getElementById('buttonScissors')
-const statusOne = document.getElementById('status')
+const startButton = document.getElementById('start')
+const input = document.getElementById('input');
+const statusOne = document.getElementById('status');
 
-const scoreboardMy = document.getElementById('zeroMy')
-const scoreboardPc = document.getElementById('zeroPc')
+document.getElementById('buttonStone').disabled = true;
+document.getElementById('buttonPaper').disabled = true;
+document.getElementById('buttonScissors').disabled = true;
+document.getElementById('start').disabled = true;
 
-const teste = 'Parabéns - Você ganhou 🏆'
-const testeteste = 'Perdeu 🤬 Tente Novamente'
+document.getElementById('input').addEventListener('input', function(event){
+    var conteudo = document.getElementById("input").value;
+    
+    if (conteudo !== null && conteudo !== ''){
+        document.getElementById('buttonStone').disabled = false;
+        document.getElementById('buttonPaper').disabled = false;
+        document.getElementById('buttonScissors').disabled = false;
+        document.getElementById('start').disabled = false;
+    } else {
+        document.getElementById('buttonStone').disabled = true;
+        document.getElementById('buttonPaper').disabled = true;
+        document.getElementById('buttonScissors').disabled = true;
+        document.getElementById('start').disabled = true;
+    }
+
+});
+
+const insertName = () => {
+    document.getElementById('start').disabled = true;
+    const myName = document.getElementById('myName');
+
+    myName.innerHTML = input.value;
+    input.value="";
+
+    statusOne.innerHTML = '🍀 Faça a sua escolha 🍀';
+
+}
 
 
 const changeImg = () => {
-    changeImgOne.src = "./Assets/pedra.png"
+     
+  
+    
+
+    document.getElementById('start').disabled = true;
+
+    const changeImgOne = document.getElementById("firstImg");
+    changeImgOne.src = "./Assets/pedra.png";
+
+    const pcImg = document.getElementById('secondImg');
+    const statusOne = document.getElementById('status');
+    const myResult = document.getElementById('zeroMy');
+    const pcResult = document.getElementById('zeroPc');
 
     const randon = Math.random()
     const number = (Math.ceil(randon * ((3 - 1) + 1)));
 
     
-
     if (number === 1) {
-        pcImg.src = "./Assets/pedra.png"
+        pcImg.src = "./Assets/pedra.png";
+        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente';
 
-    } else if (number === 2) {
-        pcImg.src = "./Assets/papel.png"
+    } 
+    if (number === 2) {
+        pcImg.src = "./Assets/papel.png";
+        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente';
 
-    } else {
-        pcImg.src = "./Assets/tesoura.png"
+        pcResult.innerHTML = `${pcResult.innerHTML} +1`;
+        pcResult.innerHTML = eval(pcResult.innerHTML);
+    } 
+
+    if (number === 3) {
+        pcImg.src = "./Assets/tesoura.png";
+        statusOne.innerHTML = 'Vitória 😁 Jogue Novamente';
+
+        myResult.innerHTML = `${myResult.innerHTML} +1`;
+        myResult.innerHTML = eval(myResult.innerHTML);
     };
 
-
-    if (number === 1) {
-        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente'
-    } else if (number === 2) {
-        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente'
-
-    } else {
-        statusOne.innerHTML = 'Vitória 😁 Jogue Novamente'
-    };
     
-            
-    
-    if (number === 1) {
-        
-    } else if (number === 2) {
-        scoreboardPc.innerHTML = '1'
+ }
 
-    } else {
-        scoreboardMy.innerHTML = '1'
-    };
 
-}
 
 const changeSecondImg = () => {
-    const changeSecondImg = document.getElementById("firstImg")
-    changeSecondImg.src = "./Assets/papel.png"
+    document.getElementById('start').disabled = true;
+    const changeImgOne = document.getElementById("firstImg");
+    changeImgOne.src = "./Assets/papel.png";
 
+    const pcImg = document.getElementById('secondImg');
+    const statusOne = document.getElementById('status');
+    const myResult = document.getElementById('zeroMy');
+    const pcResult = document.getElementById('zeroPc');
 
     const randon = Math.random()
     const number = (Math.ceil(randon * ((3 - 1) + 1)));
 
+
     if (number === 1) {
-        pcImg.src = "./Assets/pedra.png"
+        pcImg.src = "./Assets/pedra.png";
+        statusOne.innerHTML = 'Vitória 😁 Jogue Novamente';
 
-    } else if (number === 2) {
-        pcImg.src = "./Assets/papel.png"
-
-    } else {
-        pcImg.src = "./Assets/tesoura.png"
-    };
+        myResult.innerHTML = `${myResult.innerHTML} +1`;
+        myResult.innerHTML = eval(myResult.innerHTML);
+    } 
 
     if (number === 2) {
-        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente'
-    } else if (number === 1) {
-        statusOne.innerHTML = 'Vitória 😁 Jogue Novamente'
-    } else {
-        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente'
+        pcImg.src = "./Assets/papel.png";
+        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente';        
     }
 
-    if (number === 2) {
-        
-    } else if (number === 1) {
-        scoreboardMy.innerHTML = '1'
+     if (number === 3) {
+        pcImg.src = "./Assets/tesoura.png";
+        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente';
 
-    } else {
-        scoreboardPc.innerHTML = '1'
+        pcResult.innerHTML = `${pcResult.innerHTML} +1`;
+        pcResult.innerHTML = eval(pcResult.innerHTML);
     };
+
+   
+
 }
 
 const changeThirdButton = () => {
-    const changeThirdButton = document.getElementById("firstImg")
-    changeThirdButton.src = "./Assets/tesoura.png"
+    document.getElementById('start').disabled = true;
+    const changeImgOne = document.getElementById("firstImg");
+    changeImgOne.src = "./Assets/tesoura.png";
+
+    const pcImg = document.getElementById('secondImg');
+    const statusOne = document.getElementById('status');
+    const myResult = document.getElementById('zeroMy');
+    const pcResult = document.getElementById('zeroPc');
 
     const randon = Math.random()
     const number = (Math.ceil(randon * ((3 - 1) + 1)));
 
     if (number === 1) {
-        pcImg.src = "./Assets/pedra.png"
+        pcImg.src = "./Assets/pedra.png";
+        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente';
 
-    } else if (number === 2) {
+        pcResult.innerHTML = `${pcResult.innerHTML} +1`;
+        pcResult.innerHTML = eval(pcResult.innerHTML);
+    } 
+
+    if (number === 2) {
         pcImg.src = "./Assets/papel.png"
-
-    } else {
-        pcImg.src = "./Assets/tesoura.png"
-    };
-
-
-    if (number === 3) {
-        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente'
-    } else if (number === 1) {
-        statusOne.innerHTML = 'Derrota 😕 Jogue Novamente'
-    } else {
         statusOne.innerHTML = 'Vitória 😁 Jogue Novamente'
-    }
 
-
-    if (number === 3) {
-        
-    } else if (number === 1) {
-        scoreboardPc.innerHTML = '1'
-
-    } else {
-        scoreboardMy.innerHTML = '1'
+        myResult.innerHTML = `${myResult.innerHTML} +1`;
+        myResult.innerHTML = eval(myResult.innerHTML);
+    } 
+    
+    if(number === 3) {
+        pcImg.src = "./Assets/tesoura.png"
+        statusOne.innerHTML = 'Empate 🙄 Jogue Novamente'
     };
 
+      
 }
 
 
@@ -133,3 +166,4 @@ const changeThirdButton = () => {
 firstButton.addEventListener('click', changeImg)
 secondButton.addEventListener('click', changeSecondImg)
 thirdButton.addEventListener('click', changeThirdButton)
+startButton.addEventListener('click', insertName)
